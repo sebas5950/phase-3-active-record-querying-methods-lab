a3f582dc5861f143d25d572605bd401b4ee87e99
Alex Guanipatin <sebas5950@gmail.com>
2022-07-06 14:12:37 -0400

    commit tree 476c52fa6084f76e01d138f351a13c27bc2bcc75

---
+++ app/models/show.rb
@@ -0,0 +1,33 @@
+class Show < ActiveRecord::Base
+
+    def self.highest_rating
+        self.maximum(:rating)
+    end
+
+    def self.most_popular_show
+        ordering = self.order(:rating)
+        ordering.last
+    end
+
+    def  self.lowest_rating
+        self.minimum(:rating)
+    end
+
+    def self.least_popular_show
+        descOrdering = self.order(:rating)
+        descOrdering.first  
+    end
+
+    def self.ratings_sum
+        self.sum(:rating)
+    end
+
+    def self.popular_shows
+        self.where("rating > ?", 5)
+    end
+
+    def self.shows_by_alphabetical_order
+        self.order(:name)
+    end
+
+end
